a03141c4f0787b827a4fd90bafe06c5c56c6f867
Kevin Purdy <purdyk@kratos.nws.oregonstate.edu>
2008-12-02 17:23:43 -0800

    Beginning of new dashboard.

---
+++ src/com/sound/ampache/dashActivity.java
@@ -0,0 +1,16 @@
+package com.sound.ampache;
+
+import com.sound.ampache.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class dashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.dash);
+    }
+}
