e697d4e450cce51cf5762da574bac926920db161
Kevin Purdy <purdyk@kratos.nws.oregonstate.edu>
2008-12-10 13:54:20 -0800

    Added GPL Header to files.

--- src/com/sound/ampache/R.java
+++ src/com/sound/ampache/R.java
@@ -36,10 +36,9 @@ public final class R {
     public static final class layout {
         public static final int browsable_item=0x7f030000;
         public static final int dash=0x7f030001;
-        public static final int dashimages=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int playlist=0x7f030004;
-        public static final int playlist_item=0x7f030005;
+        public static final int main=0x7f030002;
+        public static final int playlist=0x7f030003;
+        public static final int playlist_item=0x7f030004;
     }
     public static final class menu {
         public static final int collection_menu=0x7f060000;
