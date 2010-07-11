780f02025256c20a5b2f2e7747d1ccf1d75f097c
Jacob Alexander <haata@users.sf.net>
2010-07-10 21:50:21 -0700

    Integration fixes for MTGap's notification patch.

--- src/com/sound/ampache/service/PlayerService.java
+++ src/com/sound/ampache/service/PlayerService.java
@@ -26,6 +26,7 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
